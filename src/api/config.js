6e84c55c27100c70a1a007b11f906d8e85463338
gugamm <gugamm@live.com>
2017-04-05 17:15:20 -0700

    update example

--- src/api/config.js
+++ src/api/config.js
@@ -1,12 +1,7 @@
 import repos from './resources/repos';
 
-const jsonParser = (response) => JSON.parse(response.data);
-
 const config = {
   host : 'https://api.github.com',
-  options : {
-    parseResponse : jsonParser
-  },
   resources : [
     repos
   ]
